f178b3d143583597810982915a4fe17d87bd79e3
BUGO07 <71205861+BUGO07@users.noreply.github.com>
2025-04-28 22:05:02 +0400

    push to git

--- build.rs
+++ build.rs
@@ -1,12 +1,64 @@
-fn main() {
-    cc::Build::new()
-        .file("flanterm/flanterm.c")
-        .file("flanterm/backends/fb.c")
-        .define("FLANTERM_FB_DISABLE_BUMP_ALLOC", "")
-        .flag("-std=c11")
+use std::process::Command;
+use std::{
+    env,
+    error::Error,
+    path::{Path, PathBuf},
+};
+
+const SOURCES: &[&str] = &["flanterm.c", "backends/fb.c"];
+
+fn init_submodule(flanterm_path: &Path) {
+    if !flanterm_path.join("README.md").exists() {
+        Command::new("git")
+            .args(["submodule", "update", "--init"])
+            .current_dir(flanterm_path)
+            .status()
+            .expect("failed to retrieve flanterm sources with git");
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let project_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let flanterm_path = Path::new(&project_dir).join("flanterm");
+
+    init_submodule(&flanterm_path);
+
+    let flanterm_path_str = flanterm_path.to_str().unwrap();
+
+    let sources = SOURCES
+        .iter()
+        .map(|file| format!("{flanterm_path_str}/{file}"));
+
+    let mut cc = cc::Build::new();
+
+    cc.files(sources)
+        .define("FLANTERM_FB_DISABLE_BUMP_ALLOC", "") // reduces binary size but needs memory allocator
+        // .flag("-std=c11")
+        // .flag("-mgeneral-regs-only")
+        .flag("-nostdlib")
         .flag("-ffreestanding")
         .flag("-fno-stack-protector")
-        .flag("-fno-stack-check")
-        .flag("-mno-red-zone")
-        .compile("flanterm")
+        .flag("-fno-stack-check");
+
+    if cfg!(target_arch = "x86_64") || cfg!(target_arch = "x86") {
+        cc.flag("-mno-red-zone");
+    }
+
+    cc.compile("flanterm");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .clang_args(&["-ffreestanding"])
+        .prepend_enum_name(false)
+        .use_core()
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,6 @@
 #![no_std]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-use core::ptr::NonNull;
-
-#[allow(nonstandard_style)]
-pub mod sys;
-
-#[repr(transparent)]
-pub struct Context {
-    raw: NonNull<sys::flanterm_context>,
-}
-
-impl Context {
-    pub fn write(&mut self, buf: &[u8]) {
-        unsafe { sys::flanterm_write(self.raw.as_ptr(), buf.as_ptr().cast(), buf.len()) }
-    }
-}
-
-impl Drop for Context {
-    fn drop(&mut self) {
-        unsafe {
-            sys::flanterm_deinit(self.raw.as_ptr(), None);
-        }
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
